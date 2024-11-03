0aca2958610f0953e5c94c1b644c7943a87335b4
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-11-03 20:07:35 +0100

    Add model Country

--- countries/models.py
+++ countries/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.CharField(max_length=100)
+    country_code = models.CharField(max_length=3, unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    group_id = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name_plural = "Countries"
+        ordering = ["name"]
