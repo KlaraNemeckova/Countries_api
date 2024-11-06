538986ce2d0710dbdcc4198057d1ef012feb35f3
KlaraNemeckova <klaranemeckova1@gmail.com>
2024-11-06 14:53:02 +0100

    Implement pagination to list of countries

--- countries/views.py
+++ countries/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
+from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
@@ -6,33 +7,41 @@ from .models import Country
 from .serializers import CountrySerializer, CountryCreateSerializer
 
 
-
 def home(request):
     return render(request, "home.html") 
-        
+
 
 @api_view(['GET', 'POST'])
 def countries_list(request):
     if request.method == 'GET':
         countries = Country.objects.all()
-        if request.accepted_renderer.format == 'html':
-            return render(request, 'countries_list.html', {'countries': countries})
+        
+        paginator = Paginator(countries, 50)  
+        page_number = request.GET.get('page')  
+        page_obj = paginator.get_page(page_number)  # stránkované výsledky, které se předají do html nebo JSON
 
-        # Jinak se vrátí JSON odpověď
-        serializer = CountrySerializer(countries, many=True)
-        return Response(serializer.data)
-    
+        if request.accepted_renderer.format == 'html':
+            return render(request, 'countries_list.html', {'page_obj': page_obj})  # vrátí stránkovaný list do countries_list.html
+
+        serializer = CountrySerializer(page_obj, many=True)
+        return Response({
+            'count': paginator.count,  
+            'offset': paginator.per_page * (page_obj.number - 1),  
+            'limit': paginator.per_page,  
+            'results': serializer.data  
+        })
+
     elif request.method == 'POST':
         if isinstance(request.data, list): 
             serializer = CountryCreateSerializer(data=request.data, many=True)
         else:
             serializer = CountryCreateSerializer(data=request.data)
-                                                                                                                          
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
+
 
 
 @api_view(['GET', 'PUT', 'DELETE'])
